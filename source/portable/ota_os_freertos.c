f32ebd3ed3f45b1d1a8323a492f698e695b0ae27
pvyawaha <pvyawaha@amazon.com>
2020-11-10 10:27:38 -0800

    Add OTA FreeRTOS port header and implementation

---
+++ source/portable/ota_os_freertos.c
@@ -0,0 +1,324 @@
+/*
+ * FreeRTOS OTA V1.2.0
+ * Copyright (C) 2020 Amazon.com, Inc. or its affiliates.  All Rights Reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * http://aws.amazon.com/freertos
+ * http://www.FreeRTOS.org
+ */
+
+/* FreeRTOS includes. */
+#include "FreeRTOS.h"
+#include "timers.h"
+#include "queue.h"
+
+/* OTA OS POSIX Interface Includes.*/
+#include "ota_os_freertos.h"
+
+/* OTA Event queue attributes.*/
+#define MAX_MESSAGES    10
+#define MAX_MSG_SIZE    sizeof( OtaEventMsg_t )
+
+/* Array containing pointer to the OTA event structures used to send events to the OTA task. */
+static OtaEventMsg_t xQueueData[ MAX_MESSAGES ];
+
+/* The queue control structure.  .*/
+static StaticQueue_t xStaticQueue;
+
+/* The queue control handle.  .*/
+QueueHandle_t xOtaEventQueue;
+
+/* The timer handle.  .*/
+TimerHandle_t xTimer;
+
+OtaErr_t OtaInitEvent_FreeRTOS( OtaEventContext_t * pEventCtx )
+{
+    ( void ) pContext;
+
+    OtaErr_t otaErrRet = OTA_ERR_UNINITIALIZED;
+
+    xOtaEventQueue = xQueueCreateStatic( ( UBaseType_t ) OTA_NUM_MSG_Q_ENTRIES,
+                                         ( UBaseType_t ) MAX_MSG_SIZE,
+                                         ( uint8_t * ) xQueueData,
+                                         &xStaticQueue );
+
+    if( xOtaEventQueue == NULL )
+    {
+        otaErrRet = OTA_ERR_EVENT_Q_CREATE_FAILED;
+
+        LogError( ( "Failed to create OTA Event Queue: "
+                    "xQueueCreateStatic returned error: "
+                    "otaErrRet=%i ",
+                    otaErrRet ) );
+    }
+    else
+    {
+        otaErrRet = OTA_ERR_NONE;
+
+        LogInfo( ( "OTA Event Queue created." ) );
+    }
+
+    return otaErrRet;
+}
+
+OtaErr_t OtaSendEvent_FreeRTOS( OtaEventContext_t * pContext,
+                                const void * pEventMsg,
+                                unsigned int timeout )
+{
+    ( void ) pContext;
+    ( void ) timeout;
+
+    OtaErr_t otaErrRet = OTA_ERR_UNINITIALIZED;
+
+    BaseType_t xRet = pdFALSE;
+
+    /* Send the event to OTA event queue.*/
+    xRet = xQueueSendToBack( xOtaEventQueue, pxEventMsg, ( TickType_t ) 0 );
+
+    if( xRet == pdTRUE )
+    {
+        otaErrRet = OTA_ERR_NONE;
+
+        LogInfo( ( "OTA Event Sent." ) );
+    }
+    else
+    {
+        otaErrRet = OTA_ERR_EVENT_Q_SEND_FAILED;
+
+        LogError( ( "Failed to send event to OTA Event Queue: "
+                    "xQueueSendToBack returned error: "
+                    "otaErrRet=%i ",
+                    otaErrRet ) );
+    }
+
+    return otaErrRet;
+}
+
+OtaErr_t OtaReceiveEvent_FreeRTOS( OtaEventContext_t * pContext,
+                                   void * pEventMsg,
+                                   uint32_t timeout )
+{
+    ( void ) pContext;
+
+    OtaErr_t otaErrRet = OTA_ERR_UNINITIALIZED;
+
+    BaseType_t xRet = pdFALSE;
+
+    /* Temp buffer.*/
+    char buff[ MAX_MSG_SIZE ];
+
+    xRet = xQueueReceive( xOtaEventQueue, &buff, portMAX_DELAY );
+
+    if( xRet == pdTRUE )
+    {
+        /* copy the data from local buffer.*/
+        memcpy( pEventMsg, buff, MAX_MSG_SIZE );
+
+        otaErrRet = OTA_ERR_NONE;
+
+        LogInfo( ( "OTA Event received" ) );
+    }
+    else
+    {
+        otaErrRet = OTA_ERR_EVENT_Q_RECEIVE_FAILED;
+
+        LogError( ( "Failed to receive event from OTA Event Queue: "
+                    "xQueueReceive returned error: "
+                    "otaErrRet=%i ",
+                    otaErrRet ) );
+    }
+
+    return otaErrRet;
+}
+
+void OtaDeinitEvent_FreeRTOS( OtaEventContext_t * pContext )
+{
+    ( void ) pContext;
+
+    /* Remove the event queue.*/
+    if( xOtaEventQueue != NULL )
+    {
+        vQueueDelete( xOtaEventQueue );
+
+        LogInfo( ( "OTA Event Queue Deleted." ) );
+    }
+}
+
+static void timerCallback( TimerHandle_t xTimer )
+{
+    OtaEventMsg_t xEventMsg = { 0 };
+
+    xEventMsg.eventId = OtaAgentEventRequestTimer;
+
+    /* Send timer event. */
+    OTA_SignalEvent( &xEventMsg );
+}
+
+OtaErr_t OtaStartTimer_FreeRTOS( OtaTimerContext_t * pTimerCtx,
+                                 const char * const pTimerName,
+                                 const uint32_t timeout,
+                                 void ( *callback )( void * ) )
+{
+    ( void ) pTimerCtx;
+
+    OtaErr_t otaErrRet = OTA_ERR_UNINITIALIZED;
+
+    BaseType_t xRet = pdFALSE;
+
+    /* If timer is not created.*/
+    if( xTimer == NULL )
+    {
+        /* Create the timer. */
+        xTimer = xTimerCreate( pTimerName,
+                               pdMS_TO_TICKS( timeout ),
+                               pdFALSE,
+                               NULL,
+                               timerCallback );
+
+        if( xTimer == NULL )
+        {
+            otaErrRet = OTA_ERR_TIMER_CREATE_FAILED;
+
+            LogError( ( "Failed to create OTA timer: "
+                        "xTimerCreate returned NULL "
+                        "otaErrRet=%i "
+                        otaErrRet ) );
+        }
+        else
+        {
+            otaErrRet = OTA_ERR_NONE;
+
+            LogInfo( ( "OTA Timer created." ) );
+
+            /* Start the timer. */
+            xRet = xTimerStart( xTimer, portMAX_DELAY );
+
+            if( xRet == pdTRUE )
+            {
+                otaErrRet = OTA_ERR_NONE;
+
+                LogInfo( ( "OTA Timer started." ) );
+            }
+            else
+            {
+                otaErrRet = OTA_ERR_TIMER_START_FAILED;
+
+                LogInfo( ( "Failed to start OTA timer." ) );
+            }
+        }
+    }
+    else
+    {
+        /* Reset the timer. */
+        xRet = xTimerStart( xTimer, portMAX_DELAY );
+
+        if( xRet == pdTRUE )
+        {
+            otaErrRet = OTA_ERR_NONE;
+
+            LogInfo( ( "OTA Timer restarted." ) );
+        }
+        else
+        {
+            otaErrRet = OTA_ERR_TIMER_START_FAILED;
+
+            LogError( ( "Failed to restart OTA timer." ) );
+        }
+    }
+
+    return otaErrRet;
+}
+
+OtaErr_t OtaStopTimer_FreeRTOS( OtaTimerContext_t * pTimerCtx )
+{
+    ( void ) pTimerCtx;
+
+    OtaErr_t otaErrRet = OTA_ERR_UNINITIALIZED;
+
+    if( xTimer != NULL )
+    {
+        /* Stop the timer. */
+        xRet = xTimerStop( xTimer, portMAX_DELAY );
+
+        if( xRet == pdTRUE )
+        {
+            otaErrRet = OTA_ERR_NONE;
+
+            LogInfo( ( "OTA Timer restarted." ) );
+        }
+        else
+        {
+            otaErrRet = OTA_ERR_TIMER_STOP_FAILED;
+
+            LogError( ( "Failed to stop OTA timer." ) );
+        }
+    }
+    else
+    {
+        LogError( ( "Failed to stop, timer is not created" ) );
+    }
+
+    return otaErrRet;
+}
+
+OtaErr_t ota_DeleteTimer( OtaTimerContext_t * pTimerCtx )
+{
+    ( void ) pTimerCtx;
+
+    OtaErr_t otaErrRet = OTA_ERR_UNINITIALIZED;
+
+    if( xTimer != NULL )
+    {
+        /* Stop the timer. */
+        xRet = xTimerDelete( xTimer, portMAX_DELAY );
+
+        if( xRet == pdTRUE )
+        {
+            otaErrRet = OTA_ERR_NONE;
+
+            LogInfo( ( "OTA Timer deleted." ) );
+        }
+        else
+        {
+            otaErrRet = OTA_ERR_TIMER_DELETE_FAILED;
+
+            LogError( ( "Failed to delete OTA timer." ) );
+        }
+    }
+    else
+    {
+        otaErrRet = OTA_ERR_TIMER_DELETE_FAILED;
+
+        LogError( ( "Failed to delete, timer is not created" ) );
+    }
+
+    return otaErrRet;
+}
+
+void * Malloc_FreeRTOS( size_t size )
+{
+    /* Use vPortMalloc.*/
+    return vPortMalloc( size );
+}
+
+void Malloc_Free( void * ptr )
+{
+    /* Use vPortFree.*/
+    vPortFree( ptr );
+}
---
+++ source/portable/ota_os_freertos.h
@@ -0,0 +1,162 @@
+/*
+ * FreeRTOS OTA V1.2.0
+ * Copyright (C) 2020 Amazon.com, Inc. or its affiliates.  All Rights Reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * http://aws.amazon.com/freertos
+ * http://www.FreeRTOS.org
+ */
+
+#ifndef _OTA_OS_FREERTOS_H_
+#define _OTA_OS_FREERTOS_H_
+
+/* Standard library include. */
+#include <stdint.h>
+
+/* OTA library interface include. */
+#include "ota_os_interface.h"
+
+/**
+ * @brief Initialize the OTA events.
+ *
+ * This function initializes the OTA events mechanism for freeRTOS platforms.
+ *
+ * @param[pEventCtx]     Pointer to the OTA event context.
+ *
+ * @return               OtaErr_t, OTA_ERR_NONE if success , other error code on failure.
+ */
+OtaErr_t OtaInitEvent_FreeRTOS( OtaEventContext_t * pEventCtx );
+
+/**
+ * @brief Sends an OTA event.
+ *
+ * This function sends an event to OTA library event handler on FreeRTOS platforms.
+ *
+ * @param[pEventCtx]     Pointer to the OTA event context.
+ *
+ * @param[pEventMsg]     Event to be sent to the OTA handler.
+ *
+ * @param[timeout]       The maximum amount of time (msec) the task should block.
+ *
+ * @return               OtaErr_t, OTA_ERR_NONE if success , other error code on failure.
+ */
+OtaErr_t OtaSendEvent_FreeRTOS( OtaEventContext_t * pEventCtx,
+                                const void * pEventMsg,
+                                unsigned int timeout );
+
+/**
+ * @brief Receive an OTA event.
+ *
+ * This function receives next event from the pending OTA events on FreeRTOS platforms.
+ *
+ * @param[pEventCtx]     Pointer to the OTA event context.
+ *
+ * @param[pEventMsg]     Pointer to store message.
+ *
+ * @param[timeout]       The maximum amount of time the task should block.
+ *
+ * @return               OtaErr_t, OTA_ERR_NONE if success , other error code on failure.
+ */
+OtaErr_t OtaReceiveEvent_FreeRTOS( OtaEventContext_t * pEventCtx,
+                                   void * pEventMsg,
+                                   uint32_t timeout );
+
+/**
+ * @brief Deinitialize the OTA Events mechanism.
+ *
+ * This function deinitialize the OTA events mechanism and frees any resources
+ * used on FreeRTOS platforms.
+ *
+ * @param[pEventCtx]     Pointer to the OTA event context.
+ *
+ * @return               OtaErr_t, OTA_ERR_NONE if success , other error code on failure.
+ */
+OtaErr_t OtaDeinitEvent_FreeRTOS( OtaEventContext_t * pEventCtx );
+
+
+/**
+ * @brief Start timer.
+ *
+ * This function starts the timer or resets it if it is already started on FreeRTOS platforms.
+ *
+ * @param[pTimerCtx]        Pointer to the timer context to start/reset.
+ *
+ * @param[pTimerName]       Timer name.
+ *
+ * @param[timeout]          Timeout for the timer.
+ *
+ * @param[callback]         Callback to be called when timer expires.
+ *
+ * @return                  OtaErr_t, OTA_ERR_NONE if success , other error code on failure.
+ */
+OtaErr_t OtaStartTimer_FreeRTOS( OtaTimerContext_t * pTimerCtx,
+                                 const char * const pTimerName,
+                                 const uint32_t timeout,
+                                 void ( * callback )( void * ) );
+
+/**
+ * @brief Stop timer.
+ *
+ * This function stops the timer on FreeRTOS platforms.
+ *
+ * @param[pTimerCtx]      Pointer to the timer context to start/reset. to stop.
+ *
+ * @return                OtaErr_t, OTA_ERR_NONE if success , other error code on failure.
+ */
+OtaErr_t OtaStopTimer_FreeRTOS( OtaTimerContext_t * pTimerCtx );
+
+/**
+ * @brief Delete a timer.
+ *
+ * This function deletes a timer for POSIX platforms.
+ *
+ * @param[pTimerCtx]        Pointer to the timer object to delete.
+ *
+ * @return                  OtaErr_t, OTA_ERR_NONE if success , other error code on failure.
+ */
+OtaErr_t OtaDeleteTimer_FreeRTOS( OtaTimerContext_t * pTimerCtx );
+
+/**
+ * @brief Allocate memory.
+ *
+ * This function allocates the requested memory and returns a pointer to it on FreeRTOS platforms.
+ *
+ * @param[size]        This is the size of the memory block, in bytes..
+ *
+ * @return             This function returns a pointer to the allocated memory, or NULL if
+ *                     the request fails.
+ */
+
+void * Malloc_FreeRTOS( size_t size );
+
+/**
+ * @brief Free memory.
+ *
+ * This function deallocates the memory previously allocated by a call to allocation
+ * function of type OtaMalloc_t on FreeRTOS platforms.
+ *
+ * @param[size]        ptr − This is the pointer to a memory block previously allocated with function
+ *                     of type OtaMalloc_t. If a null pointer is passed as an argument, no action occurs.
+ *
+ * @return             None.
+ */
+
+void Free_FreeRTOS( void * ptr );
+
+#endif /* ifndef _OTA_OS_FREERTOS_H_ */
