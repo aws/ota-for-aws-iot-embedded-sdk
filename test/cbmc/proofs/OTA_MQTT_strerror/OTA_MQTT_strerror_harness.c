60edb70159605a09a931391c6615671bb8f86aea
bujain <bujain@amazon.com>
2021-08-10 23:56:09 +0000

    Fixed formatting errors

--- test/cbmc/proofs/OTA_MQTT_strerror/OTA_MQTT_strerror_harness.c
+++ test/cbmc/proofs/OTA_MQTT_strerror/OTA_MQTT_strerror_harness.c
@@ -35,6 +35,6 @@ void OTA_MQTT_strerror_harness()
     pErrorMsg = OTA_MQTT_strerror( status );
 
     /* The caller of this function assumes that the return value is never NULL. */
-    __CPROVER_assert( pErrorMsg != NULL, 
-    "The function return is expected to be string representation of MQTT connection status and thus cannot be NULL." );
+    __CPROVER_assert( pErrorMsg != NULL,
+                      "The function return is expected to be string representation of MQTT connection status and thus cannot be NULL." );
 }
