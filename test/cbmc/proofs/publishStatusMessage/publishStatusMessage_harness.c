3cec16f7c27d6a93126ce59a5e2d48e8a90bf8fd
bujain <bujain@amazon.com>
2021-08-17 20:11:33 +0000

    Fix cbmc failures and updated comments

--- test/cbmc/proofs/publishStatusMessage/publishStatusMessage_harness.c
+++ test/cbmc/proofs/publishStatusMessage/publishStatusMessage_harness.c
@@ -1,23 +1,13 @@
 /* Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved. */
 /* SPDX-License-Identifier: Apache-2.0 */
-
-/*
- * Insert copyright notice
- */
-
 /**
  * @file publishStatusMessage_harness.c
  * @brief Implements the proof harness for publishStatusMessage function.
  */
-
-/*
- * Insert project header files that
- *   - include the declaration of the function
- *   - include the types needed to declare function arguments
- */
+/* Include headers for mqtt interface. */
 #include "ota_mqtt_private.h"
 
-#define TOPIC_JOB_STATUS_BUFFER_SIZE    222             /*!< Max buffer size for `jobs/notify-next` topic. */
+#define TOPIC_JOB_STATUS_BUFFER_SIZE    222U             /*!< Max buffer size of pBuffer in publishStatusMessage function. */
 #define OTA_STATUS_MSG_MAX_SIZE         128U            /*!< Max length of a job status message to the service. */
 
 /* Declaration of the test function with the mangled name. */
@@ -27,22 +17,30 @@ OtaMqttStatus_t __CPROVER_file_local_ota_mqtt_c_publishStatusMessage( OtaAgentCo
                                                                       uint8_t qos );
 
 /* Stubs required for the test function. */
-size_t stringBuilder( char * pBuffer,
+size_t __CPROVER_file_local_ota_mqtt_c_stringBuilder( char * pBuffer,
                       size_t bufferSizeBytes,
-                      const char * strings )
+                      const char * strings[] )
 {
     size_t stringSize;
 
-    /* The static size of the pBuffer in the unsubscribeFromDataStream function is
-     *  defined by TOPIC_JOB_STATUS_BUFFER_SIZE. Hence, the value stringSize can
-     *  never exceed TOPIC_JOB_STATUS_BUFFER_SIZE. */
+    /* pBuffer is initialized in publishStatusMessage function before passing it to the
+     * stringBuilder function and thus cannot be NULL. */
+    __CPROVER_assert( pBuffer != NULL,
+                      "Unable to use pBuffer: passed pointer value is NULL." );
 
+    /* strings is initialized publishStatusMessage function before passing it to the
+     * stringBuilder function and thus cannot be NULL. */
+    __CPROVER_assert( strings != NULL,
+                      "Unable to use strings: passed pointer value is NULL." );
+
+    /* The static size of the pBuffer in the publishStatusMessage function is
+     * defined by TOPIC_GET_STREAM_BUFFER_SIZE. Hence, the value stringSize can
+     * never exceed TOPIC_GET_STREAM_BUFFER_SIZE. */
     __CPROVER_assume( stringSize > 0 && stringSize < TOPIC_JOB_STATUS_BUFFER_SIZE );
 
     return stringSize;
 }
 
-/* This is a stub of an mqtt interface function required for the proof. */
 OtaMqttStatus_t publish( const char * const pacTopic,
                          uint16_t usTopicLen,
                          const char * pcMsg,
@@ -58,29 +56,27 @@ OtaMqttStatus_t publish( const char * const pacTopic,
 
 void publishStatusMessage_harness()
 {
-    /* Insert argument declarations */
     OtaAgentContext_t * pAgentCtx;
     OtaMqttInterface_t mqtt;
 
     OtaAgentContext_t agent;
     OtaInterfaces_t otaInterface;
 
-    char pMsg[ OTA_STATUS_MSG_MAX_SIZE ];
+    /* pMsg is declared statically in the updateJobStatus_Mqtt and passed to publishStatusMessage function. */
+    char pMsg[ OTA_STATUS_MSG_MAX_SIZE ];   
     uint32_t msgSize;
     uint8_t qos;
 
-    /* Initialize the Mqtt interface. */
-    mqtt.unsubscribe = publish;
+    /* publish reference to the mqtt function is expected to be assigned by the user and thus
+     * assumed not to be NULL. */
+    mqtt.publish = publish;
     otaInterface.mqtt = mqtt;
 
-
-    /* Initialize the agent with the interface. */
     agent.pOtaInterface = &otaInterface;
     pAgentCtx = &agent;
 
     /* The agent can never be NULL as it is defined as a global variable. */
     __CPROVER_assume( pAgentCtx != NULL );
-
 
     ( void ) __CPROVER_file_local_ota_mqtt_c_publishStatusMessage( pAgentCtx, pMsg, msgSize, qos );
 }
