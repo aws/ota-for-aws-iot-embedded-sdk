56835f38f47daed574f3fdd782181c251c743cf5
bujain <bujain@amazon.com>
2021-08-17 17:00:02 +0000

    Updated comments

--- test/cbmc/proofs/stringBuilderUInt32Decimal/stringBuilderUInt32Decimal_harness.c
+++ test/cbmc/proofs/stringBuilderUInt32Decimal/stringBuilderUInt32Decimal_harness.c
@@ -1,5 +1,6 @@
 /* Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved. */
 /* SPDX-License-Identifier: Apache-2.0 */
+
 /**
  * @file stringBuilderUInt32Decimal_harness.c
  * @brief Implements the proof harness for stringBuilderUInt32Decimal function.
@@ -17,20 +18,19 @@ size_t __CPROVER_file_local_ota_mqtt_c_stringBuilderUInt32Decimal( char * pBuffe
 
 void stringBuilderUInt32Decimal_harness()
 {
-    /* The pBuffer is used as string in various functions but the maximum size of the. */
     char * pBuffer;
     size_t bufferSizebytes;
     uint32_t value;
 
     /* The bufferSizebytes is the size of the pBuffer. The pBuffer is statically initialized with
-     *  a size of U32_MAX_LEN + 1 in all the functions which call stringBuilderUInt32Decimal. Hence,
-     *  the size can never be below that. */
+     * a size of U32_MAX_LEN + 1 in all the functions which call stringBuilderUInt32Decimal. Hence,
+     * the size can never be below that. */
     __CPROVER_assume( bufferSizebytes > U32_MAX_LEN );
 
     pBuffer = ( char * ) malloc( bufferSizebytes );
 
     /* pBuffer is always initialized statically before passing it to the function. Hence,
-     *  it can never be NULL. */
+     * it can never be NULL. */
     __CPROVER_assume( pBuffer != NULL );
 
     ( void ) __CPROVER_file_local_ota_mqtt_c_stringBuilderUInt32Decimal( pBuffer, bufferSizebytes, value );
