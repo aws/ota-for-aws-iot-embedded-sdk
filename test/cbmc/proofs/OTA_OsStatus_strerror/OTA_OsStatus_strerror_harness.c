97dbbcf4769d8eba4bcd37f0dbbfc43d010ed1ff
bujain <bujain@amazon.com>
2021-09-02 22:03:08 +0000

    Uncrustified

--- test/cbmc/proofs/OTA_OsStatus_strerror/OTA_OsStatus_strerror_harness.c
+++ test/cbmc/proofs/OTA_OsStatus_strerror/OTA_OsStatus_strerror_harness.c
@@ -19,6 +19,7 @@
  * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
  * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
+
 /**
  * @file OTA_OsStatus_strerror_harness.c
  * @brief Implements the proof harness for OTA_OsStatus_strerror function.
