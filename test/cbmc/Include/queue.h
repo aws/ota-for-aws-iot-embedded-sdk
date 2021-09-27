cccff9ead43a0508ace252e4ffc807e6eeae5592
bujain <bujain@amazon.com>
2021-09-27 12:41:32 +0000

    Fix formatting errors

--- test/cbmc/Include/queue.h
+++
@@ -1,8 +0,0 @@
-#ifndef QUEUE_H
-#define QUEUE_H
-
-/* Data structures required for the CBMC proofs of freeRTOS functions in OTA. */
-struct QueueDefinition;
-typedef struct QueueDefinition * QueueHandle_t;
-
-#endif
