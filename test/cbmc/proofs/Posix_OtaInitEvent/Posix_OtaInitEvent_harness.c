993668f6d5136a48654d2c237b1af4fe318351ae
bujain <bujain@amazon.com>
2021-10-18 21:08:15 +0000

    update copyright notice

--- test/cbmc/proofs/Posix_OtaInitEvent/Posix_OtaInitEvent_harness.c
+++ test/cbmc/proofs/Posix_OtaInitEvent/Posix_OtaInitEvent_harness.c
@@ -1,6 +1,6 @@
 /*
- * AWS IoT Over-the-air Update v3.0.0
- * Copyright (C) 2020 Amazon.com, Inc. or its affiliates.  All Rights Reserved.
+ * AWS IoT Over-the-air Update v3.1.0
+ * Copyright (C) 2021 Amazon.com, Inc. or its affiliates.  All Rights Reserved.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of
  * this software and associated documentation files (the "Software"), to deal in
@@ -19,7 +19,6 @@
  * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
  * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
-
 /**
  * @file Posix_OtaInitEvent_harness.c
  * @brief Implements the proof harness for Posix_OtaInitEvent function.
