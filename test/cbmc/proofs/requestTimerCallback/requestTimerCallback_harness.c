4d915617368832deb5dd303494af6d6f586279b7
bujain <bujain@amazon.com>
2021-10-19 23:40:32 +0000

    update license and stubs

--- test/cbmc/proofs/requestTimerCallback/requestTimerCallback_harness.c
+++ test/cbmc/proofs/requestTimerCallback/requestTimerCallback_harness.c
@@ -1,6 +1,6 @@
 /*
- * AWS IoT Over-the-air Update v3.0.0
- * Copyright (C) 2020 Amazon.com, Inc. or its affiliates.  All Rights Reserved.
+ * AWS IoT Over-the-air Update v3.1.0
+ * Copyright (C) 2021 Amazon.com, Inc. or its affiliates.  All Rights Reserved.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of
  * this software and associated documentation files (the "Software"), to deal in
@@ -27,59 +27,42 @@
 #include "ota_os_freertos.h"
 #include "FreeRTOS.h"
 #include "timers.h"
-#include "ota_private.h"
 
 /* Declaration of the mangled name function created by CBMC for static functions.*/
 void __CPROVER_file_local_ota_os_freertos_c_requestTimerCallback(TimerHandle_t timer);
 
 void otaTimerCallback( OtaTimerId_t otaTimerId )
 {
-    assert( ( otaTimerId == OtaRequestTimer ) || ( otaTimerId == OtaSelfTestTimer ) );
-
-    if( otaTimerId == OtaRequestTimer )
-    {
-        OtaEventMsg_t xEventMsg = { 0 };
-
-        LogDebug( ( "Self-test expired within %ums",
-                    otaconfigFILE_REQUEST_WAIT_MS ) );
-
-        xEventMsg.eventId = OtaAgentEventRequestTimer;
-
-        /* Send request timer event. */
-        if( OTA_SignalEvent( &xEventMsg ) == false )
-        {
-            LogError( ( "Failed to signal the OTA Agent to start request timer" ) );
-        }
-    }
-    else /* ( otaTimerId == OtaSelfTestTimer ) */
-    {
-        LogError( ( "Self test failed to complete within %ums",
-                    otaconfigSELF_TEST_RESPONSE_WAIT_MS ) );
-
-    }
-}
-
-bool OTA_SignalEvent( const OtaEventMsg_t * const pEventMsg )
-{
-    bool status;
-    return status;
+    __CPROVER_assert( ( otaTimerId == OtaRequestTimer ) || ( otaTimerId == OtaSelfTestTimer ), 
+                            "Invalid OtaTimerId: Expected OtaRequestTimer");
 }
 
 void requestTimerCallback_harness()
 {
     TimerHandle_t timer;
     OtaTimerId_t otaTimerId;
-    char * pTimerName;
+    OtaTimerCallback_t callback;
+    const char * pTimerName;
+    size_t thingNameSize;
     uint32_t timeout;
 
-    __CPROVER_assume(otaTimerId >= 0 && otaTimerId < 2);
+    pTimerName = (const char*)malloc(thingNameSize * sizeof(char));
 
-    __CPROVER_assume(timeout < UINT32_MAX / 1000);
+    callback = otaTimerCallback;
+
+    /* otaTimerId can only have values of OtaTimerId_t enumeration. */
+    __CPROVER_assume(otaTimerId == OtaRequestTimer || otaTimerId == OtaSelfTestTimer);
+
+    /* To avoid integer overflow in pdMs_TO_TICKS. */
+    __CPROVER_assume(timeout < (UINT32_MAX / configTICK_RATE_HZ));
     
+    /* OtaStartTimer_FreeRTOS initializes the function pointer OtaTimerCallback. */
     OtaStartTimer_FreeRTOS( otaTimerId,
                                  pTimerName,
                                     timeout,
-                                      otaTimerCallback );
+                                      callback );
 
     __CPROVER_file_local_ota_os_freertos_c_requestTimerCallback(timer);
+
+    free(pTimerName);
 }
