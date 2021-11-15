6c2aaa18203bd166b249923d94b845e2610d113d
bujain <bujain@amazon.com>
2021-11-15 23:16:27 +0000

    Add assert for timerStop sub

--- test/cbmc/proofs/OTA_Suspend/OTA_Suspend_harness.c
+++ test/cbmc/proofs/OTA_Suspend/OTA_Suspend_harness.c
@@ -34,7 +34,14 @@ OtaOsStatus_t timerStop( OtaTimerId_t otaTimerId )
 {
     OtaOsStatus_t status;
 
+    /* otaTimerId must only have values of OtaTimerId_t enum. */
+    __CPROVER_assert((otaTimerId == OtaRequestTimer) || (otaTimerId == OtaSelfTestTimer),
+                "Error: otaTimerId can only have values OtaRequestTimer or OtaSelfTestTimer.");
+
+    /* Status must only have values of OtaOsStatus_t enum. */
     __CPROVER_assume( ( status >= OtaOsSuccess ) && ( status <= OtaOsTimerDeleteFailed ) );
+
+    return status;
 }
 
 void OTA_Suspend_harness()
