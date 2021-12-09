c6ac8ff01c4d74019ff58c224583fa379964bb52
bujain <bujain@amazon.com>
2021-12-09 23:13:42 +0000

    add checks for pOtaBuffer to be non_null

--- source/ota.c
+++ source/ota.c
@@ -3072,6 +3072,8 @@ OtaErr_t OTA_Init( OtaAppBuffer_t * pOtaBuffer,
     ( void ) pOtaEventStrings;      /* For suppressing compiler-warning: unused variable. */
     ( void ) pOtaAgentStateStrings; /* For suppressing compiler-warning: unused variable. */
 
+    assert( pOtaBuffer != NULL );
+
     /* If OTA agent is stopped then start running. */
     if( otaAgent.state == OtaAgentStateStopped )
     {
@@ -3094,9 +3096,6 @@ OtaErr_t OTA_Init( OtaAppBuffer_t * pOtaBuffer,
          */
         otaAgent.pOtaInterface = pOtaInterfaces;
 
-        /* Initialize application buffers. */
-        initializeAppBuffers( pOtaBuffer );
-
         /* Initialize local buffers. */
         initializeLocalBuffers();
 
@@ -3113,26 +3112,36 @@ OtaErr_t OTA_Init( OtaAppBuffer_t * pOtaBuffer,
          */
         ( void ) otaAgent.pOtaInterface->os.event.init( NULL );
 
-        if( pThingName == NULL )
+        if( pOtaBuffer == NULL )
         {
-            LogError( ( "Error: Thing name is NULL.\r\n" ) );
+            LogError( ( "Error: pOtaBuffer is NULL.\r\n" ) );
         }
         else
         {
-            uint32_t strLength = ( uint32_t ) ( strlen( ( const char * ) pThingName ) );
+            /* Initialize application buffers. */
+            initializeAppBuffers( pOtaBuffer );
 
-            if( strLength <= otaconfigMAX_THINGNAME_LEN )
+            if( pThingName == NULL )
             {
-                /*
-                 * Store the Thing name to be used for topics later. Include zero terminator
-                 * when saving the Thing name.
-                 */
-                ( void ) memcpy( otaAgent.pThingName, pThingName, strLength + 1UL );
-                returnStatus = OtaErrNone;
+                LogError( ( "Error: Thing name is NULL.\r\n" ) );
             }
             else
             {
-                LogError( ( "Error: Thing name is too long.\r\n" ) );
+                uint32_t strLength = ( uint32_t ) ( strlen( ( const char * ) pThingName ) );
+
+                if( strLength <= otaconfigMAX_THINGNAME_LEN )
+                {
+                    /*
+                     * Store the Thing name to be used for topics later. Include zero terminator
+                     * when saving the Thing name.
+                     */
+                    ( void ) memcpy( otaAgent.pThingName, pThingName, strLength + 1UL );
+                    returnStatus = OtaErrNone;
+                }
+                else
+                {
+                    LogError( ( "Error: Thing name is too long.\r\n" ) );
+                }
             }
         }
 
