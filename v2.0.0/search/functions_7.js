ca2c8fd8c433b93a33a73b3f409ad1ca44f5a918
Archit Gupta <71798289+archigup@users.noreply.github.com>
2021-12-22 11:07:38 -0800

    Add old doxygen bundles to gh-pages (#410)
    * Add historical doxygen bundles
    
    * Update gh-pages doxygen index page configuration

---
+++ v2.0.0/search/functions_7.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['handlejobparsingerror_0',['handleJobParsingError',['../ota_8c.html#a9d27d20bbbd359e673cff72b0565b8b0',1,'ota.c']]],
+  ['handleselftestjobdoc_1',['handleSelfTestJobDoc',['../ota_8c.html#a04a9f7e46088412b1186014bf4040813',1,'ota.c']]],
+  ['handleunexpectedevents_2',['handleUnexpectedEvents',['../ota_8c.html#ab10de29376ce3cb56f8a2e9ac38baa89',1,'ota.c']]]
+];
---
+++ v2.0.0/search/variables_e.js
@@ -0,0 +1,10 @@
+var searchData=
+[
+  ['recv_0',['recv',['../struct_ota_event_interface__t.html#acd46139762310587bb44d8b23b58c8fe',1,'OtaEventInterface_t']]],
+  ['request_1',['request',['../struct_ota_http_interface__t.html#a87e4e6ebc94538da49d7cfe24cefab1c',1,'OtaHttpInterface_t']]],
+  ['requestfileblock_2',['requestFileBlock',['../struct_ota_data_interface__t.html#a4ceb39add093ae3e3aa2798a1e515cc1',1,'OtaDataInterface_t']]],
+  ['requestjob_3',['requestJob',['../struct_ota_control_interface__t.html#a8c2422158a814c16d698982dfbdffc86',1,'OtaControlInterface_t']]],
+  ['requestmomentum_4',['requestMomentum',['../struct_ota_agent_context__t.html#abcb47ce949d9c9874ba53c08df390665',1,'OtaAgentContext_t']]],
+  ['required_5',['required',['../struct_json_doc_param__t.html#a96709fba52845d5f5ad914c89db05989',1,'JsonDocParam_t']]],
+  ['reset_6',['reset',['../struct_ota_pal_interface__t.html#a744b9602243baf92cbe2d1e8aef3b7ec',1,'OtaPalInterface_t']]]
+];
