3f999ab33f23aa6ce408c024eb3e4230dde9dceb
ActoryOu <ousc@amazon.com>
2022-05-27 06:38:47 +0000

    Decouple build dependency with coreJSON and tinyCBOR.

--- otaFilePaths.cmake
+++ otaFilePaths.cmake
@@ -5,30 +5,6 @@
 # Files specific to the repository such as test runner, platform tests
 # are not added to the variables.
 
-# 3rdparty source files.
-include( ${CMAKE_CURRENT_LIST_DIR}/source/dependency/coreJSON/jsonFilePaths.cmake )
-
-set( TINYCBOR_SOURCES
-    "${CMAKE_CURRENT_LIST_DIR}/source/dependency/3rdparty/tinycbor/src/cborpretty.c"
-    "${CMAKE_CURRENT_LIST_DIR}/source/dependency/3rdparty/tinycbor/src/cborpretty_stdio.c"
-    "${CMAKE_CURRENT_LIST_DIR}/source/dependency/3rdparty/tinycbor/src/cborencoder.c"
-    "${CMAKE_CURRENT_LIST_DIR}/source/dependency/3rdparty/tinycbor/src/cborencoder_close_container_checked.c"
-    "${CMAKE_CURRENT_LIST_DIR}/source/dependency/3rdparty/tinycbor/src/cborerrorstrings.c"
-    "${CMAKE_CURRENT_LIST_DIR}/source/dependency/3rdparty/tinycbor/src/cborparser.c"
-    "${CMAKE_CURRENT_LIST_DIR}/source/dependency/3rdparty/tinycbor/src/cborparser_dup_string.c"
-)
-set(TINYCBOR_INCLUDE_DIRS
-    "${CMAKE_CURRENT_LIST_DIR}/source/dependency/3rdparty/tinycbor/src"
-)
-# Use C99 for tinycbor as it is incompatible with C90
-if(CMAKE_C_STANDARD LESS 99)
-    set_source_files_properties(
-        ${TINYCBOR_SOURCES}
-        PROPERTIES
-        COMPILE_FLAGS "-std=gnu99"
-    )
-endif()
-
 # OTA library source files, including 3rdparties.
 set( OTA_SOURCES
     "${CMAKE_CURRENT_LIST_DIR}/source/include/ota.h"
@@ -40,8 +16,6 @@ set( OTA_SOURCES
     "${CMAKE_CURRENT_LIST_DIR}/source/ota.c"
     "${CMAKE_CURRENT_LIST_DIR}/source/ota_interface.c"
     "${CMAKE_CURRENT_LIST_DIR}/source/ota_base64.c"
-    ${JSON_SOURCES}
-    ${TINYCBOR_SOURCES}
 )
 
 # OTA library public include directories.
@@ -53,8 +27,6 @@ set( OTA_INCLUDE_PUBLIC_DIRS
 # OTA library private include directories.
 set( OTA_INCLUDE_PRIVATE_DIRS
     "${CMAKE_CURRENT_LIST_DIR}/source"
-    ${JSON_INCLUDE_PUBLIC_DIRS}
-    ${TINYCBOR_INCLUDE_DIRS}
 )
 
 # OTA library POSIX OS porting source files.
