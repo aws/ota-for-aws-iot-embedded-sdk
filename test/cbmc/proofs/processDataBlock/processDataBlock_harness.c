31d5b856e18e1bf2f7ede066e21189a7a9ee47f5
Paul Bartell <pbartell@amazon.com>
2022-05-27 11:27:39 -0700

    Revert "fix return value in stub"
    This reverts commit 0da99da2c5906b5a3c2731c751daca0b92a887e3.

--- test/cbmc/proofs/processDataBlock/processDataBlock_harness.c
+++ test/cbmc/proofs/processDataBlock/processDataBlock_harness.c
@@ -57,7 +57,6 @@ IngestResult_t writeDataBlockToFile( OtaFileContext_t * pFileContext,
     IngestResult_t result;
 
     __CPROVER_assert( pFileContext != NULL, "Error: pFileContext cannot be NULL" );
-    __CPROVER_assume( ( result >= IngestResultUninitialized ) && ( result <= IngestResultDuplicate_Continue ) );
 
     return result;
 }
