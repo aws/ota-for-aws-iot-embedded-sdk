aef3c28cea6f59f5f661501d4a434b38f1fbecbc
ActoryOu <ousc@amazon.com>
2022-06-10 02:29:06 +0000

    Fix FreeRTOS OTA event receive.

--- source/portable/os/ota_os_freertos.c
+++ source/portable/os/ota_os_freertos.c
@@ -131,9 +131,8 @@ OtaOsStatus_t OtaReceiveEvent_FreeRTOS( OtaEventContext_t * pEventCtx,
     uint8_t buff[ sizeof( OtaEventMsg_t ) ];
 
     ( void ) pEventCtx;
-    ( void ) timeout;
 
-    retVal = xQueueReceive( otaEventQueue, &buff, portMAX_DELAY );
+    retVal = xQueueReceive( otaEventQueue, &buff, pdMS_TO_TICKS( timeout ) );
 
     if( retVal == pdTRUE )
     {
