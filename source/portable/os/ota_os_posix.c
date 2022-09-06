a8106b7ea254ec5237d24d6454d88750c01b988a
Soren Ptak <Skptak@outlook.com>
2022-09-06 12:01:43 -0700

    LTS Version Change (#453)
    * Updating versions for LTS 2.0, added relevant PRs to the CHANGELOG
    
    * Fixed submodule issue, removed extra line from CHANGELOG.md
    
    * Added a coverity README.md
    
    * Fixing an incorrect link, adding a newline to end of README
    
    Co-authored-by: Soren Ptak <skptak@amazon.com>

--- source/portable/os/ota_os_posix.c
+++ source/portable/os/ota_os_posix.c
@@ -1,5 +1,5 @@
 /*
- * AWS IoT Over-the-air Update v3.3.0
+ * AWS IoT Over-the-air Update v3.4.0
  * Copyright (C) 2020 Amazon.com, Inc. or its affiliates.  All Rights Reserved.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of
