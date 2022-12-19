0213d7225618832d2f436273ef44b572913cde86
Kody Stribrny <kstribrn@amazon.com>
2022-12-19 15:22:49 -0800

    Add doxygen comment

--- source/ota_mqtt.c
+++ source/ota_mqtt.c
@@ -48,7 +48,7 @@
 #define OTA_CLIENT_TOKEN                      "rdy"         /*!< Arbitrary client token sent in the stream "GET" message. */
 
 /* Maximum length of ThingName appended onto the ClientToken*/
-#define OTA_CLIENT_TOKEN_MAX_THINGNAME_LEN    53 /* Max client token length (64) - Max request count length (10) - 1 (colon separator). */
+#define OTA_CLIENT_TOKEN_MAX_THINGNAME_LEN    53 /*<!  Max ThingName length = Max client token length (64) - Max request count length (10) - 1 (colon separator) */
 
 /* Agent to Job Service status message constants. */
 #define OTA_STATUS_MSG_MAX_SIZE               128U      /*!< Max length of a job status message to the service. */
