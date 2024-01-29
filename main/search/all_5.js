1b146868fd8550db9d8454f8db3e4fd859b75fc8
aggarg <aggarg@users.noreply.github.com>
2024-01-29 07:18:59 +0000

    Doxygen for 2557f84b8295a30306aced925048d1073dcde968

--- main/search/all_5.js
+++ main/search/all_5.js
@@ -6,9 +6,10 @@ var searchData=
   ['fileindex_3',['fileIndex',['../struct_ota_agent_context__t.html#aaedb31086304fd6cc93bcd03de9dbf24',1,'OtaAgentContext_t']]],
   ['filepathmaxsize_4',['filePathMaxSize',['../struct_ota_file_context__t.html#a011bd760168a0265d0912bfb9ae5b35a',1,'OtaFileContext_t']]],
   ['filesize_5',['fileSize',['../struct_ota_file_context__t.html#a26d56a7c06c041a73e569ca53e71ac67',1,'OtaFileContext_t']]],
-  ['filetype_6',['fileType',['../struct_ota_file_context__t.html#a3b880b7e8fcb4fee11607ac8bd454915',1,'OtaFileContext_t']]],
-  ['filetypeid_7',['fileTypeId',['../struct_ota_job_document__t.html#a684a4b443b542fc72c65318a2ed86b92',1,'OtaJobDocument_t']]],
-  ['free_8',['free',['../struct_ota_malloc_interface__t.html#a3de64b7780a2c70f182d4a64911dc476',1,'OtaMallocInterface_t']]],
-  ['freefilecontextmem_9',['freeFileContextMem',['../ota_8c.html#ae466af59782893adbc54667423fe266f',1,'ota.c']]],
-  ['functions_10',['Functions',['../ota_functions.html',1,'']]]
+  ['filesizetoblocks_6',['fileSizeToBlocks',['../ota_8c.html#af07f5a44a626cfb04eaf9d8938c3b18b',1,'ota.c']]],
+  ['filetype_7',['fileType',['../struct_ota_file_context__t.html#a3b880b7e8fcb4fee11607ac8bd454915',1,'OtaFileContext_t']]],
+  ['filetypeid_8',['fileTypeId',['../struct_ota_job_document__t.html#a684a4b443b542fc72c65318a2ed86b92',1,'OtaJobDocument_t']]],
+  ['free_9',['free',['../struct_ota_malloc_interface__t.html#a3de64b7780a2c70f182d4a64911dc476',1,'OtaMallocInterface_t']]],
+  ['freefilecontextmem_10',['freeFileContextMem',['../ota_8c.html#ae466af59782893adbc54667423fe266f',1,'ota.c']]],
+  ['functions_11',['Functions',['../ota_functions.html',1,'']]]
 ];
--- main/search/functions_5.js
+++ main/search/functions_5.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['freefilecontextmem_0',['freeFileContextMem',['../ota_8c.html#ae466af59782893adbc54667423fe266f',1,'ota.c']]]
+  ['filesizetoblocks_0',['fileSizeToBlocks',['../ota_8c.html#af07f5a44a626cfb04eaf9d8938c3b18b',1,'ota.c']]],
+  ['freefilecontextmem_1',['freeFileContextMem',['../ota_8c.html#ae466af59782893adbc54667423fe266f',1,'ota.c']]]
 ];
